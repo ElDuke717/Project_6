b5c70eb33e5b5f4a4a6be456f426a989c247c8e6
Nick Huemmer <njhuemmer@gmail.com>
2020-10-07 16:32:27 -0700

    Now it's connected and working right
    It's working right.  probably better to stick with the GUI GitHub desktop instead of using the command line for now.

--- game_show_app_v1.2/js/app.js
+++ game_show_app_v1.2/js/app.js
@@ -2,6 +2,8 @@ const querty = document.getElementById('querty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
 
+//first stab at the first function - I have to review JavaScript and the DOM. 
+
 function score() {
     let gameState;
     if (missed = 0) {
