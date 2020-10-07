eab75abfa061bd218e34ba3ed788c79fc1303593
Nick Huemmer <njhuemmer@gmail.com>
2020-10-07 16:26:53 -0700

    Initial Commit
    Had some connection issues with the first version of the repo, so will try this one instead.  Project_6.

---
+++ game_show_app_v1.2/js/app.js
@@ -0,0 +1,12 @@
+const querty = document.getElementById('querty');
+const phrase = document.getElementById('phrase');
+const missed = 0;
+
+function score() {
+    let gameState;
+    if (missed = 0) {
+        gameState = "keep playing";
+    } else (missed = 5) {
+        gameState = "Game Over";
+    }
+}
