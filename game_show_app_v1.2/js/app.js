ab403408d6ee9f4212d917b43d8c16f729b1b712
Nick Huemmer <njhuemmer@gmail.com>
2020-10-08 14:32:46 -0700

    Started Work
    Added more functionality and starting to build out the scripts.

--- game_show_app_v1.2/js/app.js
+++ game_show_app_v1.2/js/app.js
@@ -1,14 +1,60 @@
-const querty = document.getElementById('querty');
+const querty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const btnReset = document.querySelector('.btn__reset');
+const startScreen = document.querySelector(".start");
 const missed = 0;
+
+const phrases = [
+    "the first item",
+    "the second item",
+    "the third item",
+    "the fourth item"
+];
+
+
+
+//return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+}
+
+//adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+//check if a letter is in the phrase
+const checkLetter = button => {
+
+}
+
+//check if the game has been won or lost
+const checkWin = () => {
+
+}
+
+//listen for the start game button to be pressed
+// startButton.addEventListener('click', () => {
+
+// });
+
+//listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+
+});
+
+btnReset.addEventListener('click', () => {
+    startScreen.style.display = "none";
+});
 
 //first stab at the first function - I have to review JavaScript and the DOM. 
 
-function score() {
-    let gameState;
-    if (missed = 0) {
-        gameState = "keep playing";
-    } else (missed = 5) {
-        gameState = "Game Over";
-    }
-}
+// function score() {
+//     let gameState;
+//     if (missed = 0) {
+//         gameState = "keep playing";
+//     } else (missed <= 5) {
+//         gameState = "Game Over";
+//     }
+// }
+
