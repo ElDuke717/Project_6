90a1710261601de2d73cecbc51d9f673378b6e5b
Nick Huemmer <njhuemmer@gmail.com>
2020-10-09 16:53:23 -0700

    Added First Major Functionality
    The getRandomPhraseAsArray function and addPhraseToDisplay functions work.  Each phrase is randomly selected and displayed and I'm now starting to figure out how to display each random letter on the page.  After that, I'll have to figure out how to compare letters with those selected by the buttons.

--- game_show_app_v1.2/js/app.js
+++ game_show_app_v1.2/js/app.js
@@ -5,28 +5,58 @@ const startScreen = document.querySelector(".start");
 const missed = 0;
 
 const phrases = [
-    "the first item",
-    "the second item",
-    "the third item",
-    "the fourth item"
+    "Coronavirus Pandemic",
+    "Presidential Election",
+    "West Coast Wildfires",
+    "US Based Crewed Spaceflight",
+    "Phosphine on Venus",
+    "RIP Eddie Van Halen",
+    "Social Distancing",
+    "What a mess"
 ];
-
 
 
 //return a random phrase from an array
 const getRandomPhraseAsArray = arr => {
-
+    let rand = (Math.floor(Math.random(phrases.length)*phrases.length));
+    return phrases[rand];
 }
+
+//console.log(getRandomPhraseAsArray());
+
+
 
 //adds the letters of a string to the display
 const addPhraseToDisplay = arr => {
-
+    const text = getRandomPhraseAsArray();
+    const ul = document.getElementById('phrase');
+    const li = document.createElement('li');
+    li.textContent = text.split('');
+    ul.appendChild(li);
+    li.classList.add("liphrase");
+    // let checkli = document.querySelector('.liphrase').textContent;
+    // let split = checkli.split("");
+    // checkli = split;
+    
 }
+
+//Add the phrase to the page.
+addPhraseToDisplay();
+console.log(addPhraseToDisplay());
+
 
 //check if a letter is in the phrase
 const checkLetter = button => {
-
+    let checkli = document.querySelector('.liphrase').textContent;
+    let match;
+    //for (let i < 0; li.length; i++)
 }
+
+//Testing to see if text can be pulled from list item that was created.
+// let checkli = document.querySelector('.liphrase').textContent;
+// console.log(checkli);
+// let split = checkli.split("");
+// console.log(split);
 
 //check if the game has been won or lost
 const checkWin = () => {
@@ -47,14 +77,5 @@ btnReset.addEventListener('click', () => {
     startScreen.style.display = "none";
 });
 
-//first stab at the first function - I have to review JavaScript and the DOM. 
 
-// function score() {
-//     let gameState;
-//     if (missed = 0) {
-//         gameState = "keep playing";
-//     } else (missed <= 5) {
-//         gameState = "Game Over";
-//     }
-// }
 
---
+++ game_show_app_v1.2/js/jsplayground.js
@@ -0,0 +1,21 @@
+//This is where I store all the code I'm experimenting and playing around with. It helps me figure things out.
+
+
+/*This tests to make sure that my function works and generates a 
+random number each time that is > 0 and <=5.
+// for (let i=0; i < 100; i++) {
+// let userRand = (Math.floor(Math.random(phrases.length)*5));
+// console.log(userRand);
+// }*/
+
+//first stab at the first function - I have to review JavaScript and the DOM. 
+
+// function score() {
+//     let gameState;
+//     if (missed = 0) {
+//         gameState = "keep playing";
+//     } else (missed <= 5) {
+//         gameState = "Game Over";
+//     }
+// }
+
